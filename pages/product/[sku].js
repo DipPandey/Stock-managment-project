59c9722a3434f4999182bbb3bbc36bbe878afa9b
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-08-12 17:44:06 +1000

    push new product details page for seach

--- pages/product/[sku].js
+++ pages/product/[sku].js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Sidebar from '../../components/Sidebar';
+import '../../app/globals.css';
 
 const ProductDetail = () => {
     const router = useRouter();
